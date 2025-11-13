8685f8db8790aa573840f1d2f2ce1e7d04a6e50e
unknown <jacekwalczak4@gmail.com>
2025-11-13 19:40:54 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PsychoAssistantSoul",              # nazwa pakietu
-    version="0.1.0",                 # wersja pakietu
+    version="0.2.0",                 # wersja pakietu
     packages=find_packages(),        # automatyczne znajdowanie modułów w folderze
     install_requires=[
         # core
